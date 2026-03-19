65b8d7f7d5ed9861bbef1084b94e732dbd852f70
Jorge Hernández Pérez <jorcarherpe@gmail.com>
2026-03-19 20:25:31 +0000

    Add files via upload

---
+++ ut5/a2/calculadora/src/test/java/CalculadoraNotasTest.java
@@ -0,0 +1,17 @@
+import com.example.*;
+import static org.testng.AssertJUnit.assertEquals;
+import org.testng.annotations.Test;
+
+
+public class CalculadoraNotasTest {
+
+    @Test
+    void testMediaSimple(){
+        assertEquals(7, CalculadoraNotas.calcularMedia(new int[]{6,7,8}));
+    }
+
+    @Test
+    void testMediaListaVacia(){
+        assertEquals(new IllegalArgumentException(), CalculadoraNotas.calcularMedia(new int[]{}));
+    }
+}
